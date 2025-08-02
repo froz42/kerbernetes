385adb2d5d05cab1d9da0e94af337bac81a8221b
froz <froz@42paris.fr>
2025-08-02 21:08:21 +0200

    refactor: remove unused configuration fields from Config struct

--- internal/services/config/config.service.go
+++ internal/services/config/config.service.go
@@ -11,33 +11,8 @@ import (
 
 // Config represents the configuration options for the service.
 type Config struct {
-	LogLevel string `mapstructure:"LOG_LEVEL" default:"INFO" validate:"required"`
-
 	HTTPPort  int    `mapstructure:"HTTP_PORT"  default:"3000" validate:"required"`
 	APIPrefix string `mapstructure:"API_PREFIX" default:"/api" validate:"required"`
-
-	CookieSecure bool `mapstructure:"COOKIE_SECURE" default:"false"`
-
-	JWTSecret      string `mapstructure:"JWT_SECRET"                validate:"required"`
-	JWTExp         int    `mapstructure:"JWT_EXPIRATION" default:"3600" validate:"required"`
-	RBACConfigPath string `mapstructure:"RBAC_CONFIG_PATH" default:"./configs/rbac.yaml" validate:"required"`
-
-	DBHost string `mapstructure:"DB_HOST" validate:"required"`
-	DBPort string `mapstructure:"DB_PORT" validate:"required"`
-	DBUser string `mapstructure:"DB_USER" validate:"required"`
-	DBPass string `mapstructure:"DB_PASS" validate:"required"`
-	DBName string `mapstructure:"DB_NAME" validate:"required"`
-
-	ValkeyAddress string `mapstructure:"VALKEY_ADDRESS" validate:"required"`
-
-	OpenIDIssuer         string `mapstructure:"OPENID_ISSUER" validate:"required"`
-	ConsentDayExpiration int    `mapstructure:"CONSENT_DAY_EXPIRATION" default:"30" validate:"required"`
-	LoginURL             string `mapstructure:"LOGIN_URL" validate:"required"`
-
-	IntraTokenURL     string `mapstructure:"INTRA_TOKEN_URL" validate:"required"`
-	IntraAPIURL       string `mapstructure:"INTRA_API_URL" validate:"required"`
-	IntraClientID     string `mapstructure:"INTRA_CLIENT_ID" validate:"required"`
-	IntraClientSecret string `mapstructure:"INTRA_CLIENT_SECRET" validate:"required"`
 }
 
 // ConfigService is the interface for the config service.
