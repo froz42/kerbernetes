73a7d7e2f55d79d0036ce908973d614c66572055
froz <froz@42paris.fr>
2025-08-04 15:59:35 +0200

    feat: implement AuthAccount method in auth service to delegate authentication to K8s service

--- internal/services/auth/auth.service.go
+++ internal/services/auth/auth.service.go
@@ -1,29 +1,43 @@
 package authsvc
 
 import (
+	"context"
+
 	configsvc "github.com/froz42/kerbernetes/internal/services/config"
+	k8ssvc "github.com/froz42/kerbernetes/internal/services/k8s"
+	k8smodels "github.com/froz42/kerbernetes/internal/services/k8s/models"
 	"github.com/samber/do"
 )
 
 type AuthService interface {
+	AuthAccount(ctx context.Context, username string) (*k8smodels.Credentials, error)
 }
 
 type authService struct {
 	configsvc configsvc.ConfigService
+	k8sSvc    k8ssvc.K8sService
 }
 
 func NewProvider() func(i *do.Injector) (AuthService, error) {
 	return func(i *do.Injector) (AuthService, error) {
 		return New(
 			do.MustInvoke[configsvc.ConfigService](i),
+			do.MustInvoke[k8ssvc.K8sService](i),
 		)
 	}
 }
 
 func New(
 	configService configsvc.ConfigService,
+	k8sService k8ssvc.K8sService,
 ) (AuthService, error) {
 	return &authService{
 		configsvc: configService,
+		k8sSvc:    k8sService,
 	}, nil
+}
+
+func (s *authService) AuthAccount(ctx context.Context, username string) (*k8smodels.Credentials, error) {
+	// Delegate the authentication to the K8s service
+	return s.k8sSvc.AuthAccount(ctx, username)
 }
